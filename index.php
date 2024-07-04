f867dc8e5543c26fb64e167c5d31414bbac9f5e5
Ale Hortas <hortasgutieerrezale@gmail.com>
2024-07-04 12:52:40 -0300

    Welcome page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies DB</title>
+</head>
+<body>
+    <h1>Welcome 🌞</h1>
+</body>
+</html>
